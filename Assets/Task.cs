179ed4708757291f4508e323566721a5d0bceea6
cylina ho <hocylina@gmail.com>
2026-04-28 11:31:04 +0800

    update changes

--- Assets/Task.cs
+++ Assets/Task.cs
@@ -2,31 +2,27 @@ using UnityEngine;
 
 public class Task : MonoBehaviour
 {
-    // The variables are outside Update so they don't reset
     float move = 0.05f;
     float rotate = 1.0f;
-    
-    // We create our own angle tracker
-    float currentAngle = 0f;
-
+    void Start()
+    {
+        
+    }
     void Update()
     {
         // --- Task 1: Movement ---
         transform.position += new Vector3(move, 0, 0);
 
-        // --- Task 2: Movement Boundary ---
+        // --- Task 2: Move Boundary ---
         if (transform.position.x > 5f) move = -0.05f;
         if (transform.position.x < -5f) move = 0.05f;
 
-        // --- Task 3: Rotation (Simple Counter) ---
-        // 1. Update our own angle variable
-        currentAngle += rotate;
+        // --- Task 3: Rotation ---
+        transform.Rotate(0, rotate, 0);
 
-        // 2. Apply that angle to the object
-        transform.rotation = Quaternion.Euler(0, currentAngle, 0);
-
-        // 3. Rotation Boundary
-        if (currentAngle > 45f) rotate = -1.0f;
-        if (currentAngle < -45f) rotate = 1.0f;
+        // Rotation Boundary
+        // Using the raw Y rotation value (decimal between -1 and 1)
+        if (transform.rotation.y > 0.3f) rotate = -1.0f;
+        if (transform.rotation.y < -0.3f) rotate = 1.0f;
     }
 }
